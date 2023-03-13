335ae7b4d3befa63aa3087b64d1eca28e747cfdd
Anandhu Ashok <80965879+anandhx@users.noreply.github.com>
2023-03-13 21:50:40 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/anandhx/Instagram-Giveaway-Winner.git
+git clone https://github.com/anandhx/Instagram-Giveaway-Winner-.git
 cd Instagram-Giveaway-Winner-
 ls
 python main.py
